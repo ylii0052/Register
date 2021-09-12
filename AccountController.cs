8c228f0f0640b580379eeb059002c0e4a96bb6c0
ylii0052 <81316403+ylii0052@users.noreply.github.com>
2021-09-13 04:06:06 +1000

    Initial commit

---
+++ AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_project.Models;
+
+namespace FIT5032_project.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id=0)
+        {
+            Account account = new Account();
+            return View(account);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Account account)
+        {
+            using (DBModels db = new DBModels())
+            {
+                db.Accounts.Add(account);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful";
+            return View("AddOrEdit",new Account());
+        }
+    }
+}
